4848e17beac8e79fe29c80d139d1d39f1c65a8b3
Jonathan Sansó <jonasans2@live.com.ar>
2026-02-05 01:10:57 -0300

    chore: ignore dist-tests output

--- backend/dist-tests/src/modules/policies/domain/rules/BusinessRule.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BusinessRule = void 0;
-class BusinessRule {
-}
-exports.BusinessRule = BusinessRule;
--- backend/dist-tests/src/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const env_1 = require("./config/env");
-const logger_1 = require("./config/logger");
-const app = (0, app_1.buildApp)();
-app.listen(env_1.env.PORT, () => {
-    logger_1.logger.info({ port: env_1.env.PORT }, "api listening");
-});
